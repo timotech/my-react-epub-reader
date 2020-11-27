fa840b9d9d7b28ab8ce4626d90dd45c06b09cca0
Timothy Ayodele <timotech@yahoo.com>
2020-11-27 10:22:25 +0100

    initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { ReactReader } from "react-reader";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div style={{ position: "relative", height: "100%", width: "100%" }}>
+        {" "}
+        {/* Container needs a height.. */}
+        <ReactReader
+          url={"/Basic Technology for Junior  Secondary Schools, Book 3.epub"}
+          title={"Alice in wonderland"}
+          location={0}
+          locationChanged={(epubcifi) => console.log(epubcifi)}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
